8c81ae771fffa6cddda8c6373244ed5ec9e3c6aa
grrotterr <grrotterr@gmail.com>
2026-05-24 14:34:35 +0200

    Clean up form handling and improve readability
    Refactor form population and submission logic for clarity.

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,14 +6,11 @@ let formData = {
 const form = document.querySelector(".feedback-form");
 const STORAGE_KEY = "feedback-form-state";
 
-
 function populateForm() {
   const savedData = localStorage.getItem(STORAGE_KEY);
   
   if (savedData) {
     formData = JSON.parse(savedData);
-    
-
     form.elements.email.value = formData.email || "";
     form.elements.message.value = formData.message || "";
   }
@@ -38,12 +35,10 @@ form.addEventListener("input", (event) => {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-
   if (!formData.email || !formData.message) {
     alert("Fill please all fields");
     return;
   }
-
 
   console.log(formData);
 
@@ -53,5 +48,5 @@ form.addEventListener("submit", (event) => {
   form.reset();
 });
 
-
+// === Викликаємо ініціалізацію ПЕРЕД слухачами ===
 populateForm();
