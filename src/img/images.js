813e13a910e21a54be16583a9fa6b854bf8ceb28
grrotterr <grrotterr@mail.com>
2026-05-24 13:37:24 +0200

    hw_9

---
+++ src/img/images.js
@@ -0,0 +1,23 @@
+export const images = [
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/06/19/18/24/landscape-4285517__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/06/19/18/24/landscape-4285517_1280.jpg',
+    description: 'Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/11/03/20/11/portfolio-4597747__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/11/03/20/11/portfolio-4597747_1280.jpg',
+    description: 'Laptop',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/11/02/14/39/landscape-4593909__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/11/02/14/39/landscape-4593909_1280.jpg',
+    description: 'River',
+  },
+];
